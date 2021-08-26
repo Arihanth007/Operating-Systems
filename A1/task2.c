86317a7d3a7249b08fae0d9a5380cf604537dc5e
Arihanth Srikar <arihanthsrikar@gmail.com>
2021-08-26 14:54:11 +0530

    task2 completed and modified task1

--- A1/task2.c
+++ A1/task2.c
@@ -19,6 +19,45 @@ void reverse_string(char *c, int n)
     }
 }
 
+int char_to_int(char c)
+{
+    int n;
+    switch (c)
+    {
+    case '1':
+        n = 1;
+        break;
+    case '2':
+        n = 2;
+        break;
+    case '3':
+        n = 3;
+        break;
+    case '4':
+        n = 4;
+        break;
+    case '5':
+        n = 5;
+        break;
+    case '6':
+        n = 6;
+        break;
+    case '7':
+        n = 7;
+        break;
+    case '8':
+        n = 8;
+        break;
+    case '9':
+        n = 9;
+        break;
+    default:
+        n = 0;
+        break;
+    }
+    return n;
+}
+
 void print_progress(long long int total_char, long long int cur_char, char *p)
 {
     if (cur_char > total_char)
@@ -34,7 +73,7 @@ int main(int argc, char *argv[])
 {
     int fd, fd2, sz = 1024 * 128; // 128kb chunks
     const char *f1 = argv[1];
-    char c[sz * sizeof(char)], p[100], dir_path[500] = "Assignment/1_";
+    char c[sz * sizeof(char)], p[100], dir_path[500] = "Assignment/2_";
     strcat(dir_path, f1);
 
     mkdir("Assignment", 0777);
@@ -59,14 +98,16 @@ int main(int argc, char *argv[])
         exit(EXIT_FAILURE);
     }
 
+    int a = char_to_int(*argv[2]), b = char_to_int(*argv[3]);
     const int chunk = sz;
     off_t tmp, lst, start, end;
-    long long int div = st.st_size, to_read;
-    start = 0;
-    end = st.st_size;
+    long long int div = st.st_size / a, to_read;
+    start = (b - 1) * div;
+    end = start + div;
     to_read = end - start;
     tmp = lseek(fd, end - chunk, SEEK_SET);
 
+    // printf("a = %d, b = %d\n", a, b);
     // printf("Char in file = %lld\n", st.st_size);
     // printf("Start = %lld, End = %lld\n", start, end);
     // printf("tmp = %lld\n", tmp);
