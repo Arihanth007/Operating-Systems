e71091a9057816a8dd24f8f088504b7eae9140b6
Arihanth Srikar <arihanthsrikar@gmail.com>
2021-10-25 11:01:36 +0530

    task one completed

---
+++ xv6-riscv-riscv/user/strace.c
@@ -0,0 +1,26 @@
+#include "kernel/param.h"
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+
+int main(int argc, char *argv[])
+{
+    int EXIT_FAILURE = 1, EXIT_SUCCESS = 0, STDERR = 2;
+    // check for correct command from user
+    if (argc <= 2 || (argv[1][0] < '0' || argv[1][0] > '9'))
+    {
+        fprintf(STDERR, "strace usage: strace <mask(int[0,9])> <command[args]>\n");
+        exit(EXIT_FAILURE);
+    }
+
+    strace(atoi(argv[1]));
+
+    // copy the command to a new array
+    char *new_args[MAXARG];
+    int i;
+    for (i = 0; i < argc && i < MAXARG; i++)
+        new_args[i] = argv[i + 2];
+
+    exec(new_args[0], new_args);
+    exit(EXIT_SUCCESS);
+}
